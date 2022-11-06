069e2e4cc24a642984a3b5339ba8b25616358d6d
catab1e <tilektursunbaiuulu@gmail.com>
2022-11-06 12:39:53 +0600

    added parallax

---
+++ src/components/Something.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Something = () => {
+    return (
+        <>
+            <div className='container'>
+                <div className='container2'>
+                    <h1>Something</h1>
+                    <small>Lorem, ipsum dolor.</small>
+                </div>
+            </div>
+        </>
+        
+    );
+};
+
+export default Something;
